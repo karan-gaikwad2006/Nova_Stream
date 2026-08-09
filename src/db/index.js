86d204b584141c6f1feaf775e7b246cbdac3522e
Karan Gaikwad <karangaikwad.krg@gmail.com>
2026-08-09 22:38:19 +0530

    Production level database connection

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import express from "express";
+const app = express();
+import {DB_NAME} from "../constants.js";
+
+const connectDB = async () => {
+    try{
+      const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+      console.log(`\nMongoDB connected: ${connectionInstance}\n`); //here we can write connectionInstance.connection.host to get the host name of the database it is to check whether it is connected to the correct database or not. It will return the host name of the database which is connected to the application.
+    }catch(err){
+        console.log("MONGODB CONNECTION ERROR",err);
+        process.exit(1);
+    }
+}
+
+export default connectDB; 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 import mongoose from "mongoose";
 import express from "express";
 const app = express();
-import {DB_NAME} from "./constants.js";
+// import {DB_NAME} from "./constants.js";
 
+dotenv.config({
+    path : "./.env"
+})
+
+connectDB();
+
+//This is the first approach of doing the
+
+//This is an IIFE (Immediately Invoked Function Expression) which is used to connect to the database and start the server. It is an async function which allows us to use await for asynchronous operations like connecting to the database. If there is any error during the connection or starting the server, it will be caught in the catch block and logged to the console.
+
+/*
 (async ()=>{
     try{
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
@@ -18,3 +31,10 @@ import {DB_NAME} from "./constants.js";
         throw err;
     }
 })()
+*/
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+});
+app.listen(process.env.PORT,()=>{
+            console.log(`Server is running on port ${process.env.PORT}`);
+});
