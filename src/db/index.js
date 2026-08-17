f1529603743d655543a8ce9e71775f4a8d6f7afe
Karan Gaikwad <karangaikwad.krg@gmail.com>
2026-08-17 23:49:43 +0530

    added error for database connection in index.js

--- src/db/index.js
+++ src/db/index.js
@@ -3,6 +3,7 @@ import express from "express";
 const app = express();
 import {DB_NAME} from "../constants.js";
 
+//Configuration for database connection
 const connectDB = async () => {
     try{
       const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
--- src/index.js
+++ src/index.js
@@ -6,13 +6,25 @@ const app = express();
 // import {DB_NAME} from "./constants.js";
 
 dotenv.config({
-    path : "./.env"
+    path: "./.env"
 })
 
-connectDB();
+connectDB() //As connectDB is an async function therefore async function returns a promise so we can use .then() and .catch()
+    .then(() => {
+        app.on("error", (err) => {
+            console.error("Error", err); //this is also a listner for error if app is not able to connect to the database
+            throw err;
+        });
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port: ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("Mongo DB connection failed !!!", err);
+    })
+
 
 //This is the first approach of doing the
-
 //This is an IIFE (Immediately Invoked Function Expression) which is used to connect to the database and start the server. It is an async function which allows us to use await for asynchronous operations like connecting to the database. If there is any error during the connection or starting the server, it will be caught in the catch block and logged to the console.
 
 /*
@@ -20,7 +32,7 @@ connectDB();
     try{
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         app.on("error",(err)=>{
-            console.error("Error",err); //this is also a listner for error in app is not able to connect to the database
+            console.error("Error",err); //this is also a listner for error if app is not able to connect to the database
             throw err;
         });
         app.listen(process.env.PORT,()=>{
@@ -32,9 +44,9 @@ connectDB();
     }
 })()
 */
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Hello World");
 });
-app.listen(process.env.PORT,()=>{
-            console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
