1716a527cb157cf0f572150f451ac80ce63b1b37
Karan Gaikwad <karangaikwad.krg@gmail.com>
2026-09-14 00:56:11 +0530

    Add user-related routes for password change, account update, avatar, cover image, current user, and watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,16 @@
 import {Router} from 'express';
-import {loginUser, logoutUser, registerUser, refreshAccessToken} from '../controllers/user.controller.js';
+import {loginUser,
+        logoutUser, 
+        registerUser, 
+        refreshAccessToken, 
+        changeCurrentPassword, 
+        getCurrentUser, 
+        updateAccountDetails, 
+        updateUserAvatar, 
+        updateUserCoverImage, 
+        getUserChannelProfile, 
+        getWatchHistory
+    } from '../controllers/user.controller.js';
 
 import {upload} from '../middlewares/multer.middleware.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
@@ -29,4 +40,20 @@ router.route("/logout").post(verifyJWT, logoutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)
 
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser) //here we are only getting data we are not posting anything so get rquest
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+
+router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)  //in this we are using params in controller so we did :username in route as there we have written {username} which is named call
+
+router.route("/history").get(verifyJWT, getWatchHistory)
+
+
+router.route("")
 export default router;
