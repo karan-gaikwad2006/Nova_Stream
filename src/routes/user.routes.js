4cf0d2794f50f72092cb5085d1c9bf98b49e5029
Karan Gaikwad <karangaikwad.krg@gmail.com>
2026-09-14 01:10:25 +0530

    Fix typo in cover image upload route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -48,7 +48,7 @@ router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
 
-router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile)  //in this we are using params in controller so we did :username in route as there we have written {username} which is named call
 
